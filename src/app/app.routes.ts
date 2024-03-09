2bbc77a78699f61a1f02e172450c30088877a6e9
Markus <markblake190399@gmail.com>
2024-03-09 23:17:51 +0100

    funciona el search

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,9 @@ import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
 
-    { path: '', component: HomeComponent },
-    { path: 'clima', component: ClimaComponent },
-    { path: 'peliculas', component: PeliculasComponent },
+    { path: '', component: HomeComponent, title: 'Inicio' },
+    { path: 'clima', component: ClimaComponent, title: 'Clima' },
+    { path: 'peliculas', component: PeliculasComponent, title: 'Peliculas' },
 
     { path: '**', redirectTo: '', pathMatch: 'full' },
 
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,45 @@
-
+// Se importar NgModule para utilizar los componentes de Ng con Bootstrap
 import { Component } from '@angular/core';
+
+// Sirve para redirigir a las paginas
 import { RouterLink } from '@angular/router';
+
+// Hacemos la importacion del injector del buscador para que pueda buscar entre las paginas
+import { ComponentSearchService } from '../../services/buscador.service';
+import { FormsModule } from '@angular/forms';
+
+// Importamos Observable y Subject para que cuando busquemos entre los componentes el observable se actualice cuando cambie el searchTerm
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
+  //Declaramos el FormsModule para poder redigir a otras paginas
+  //Despues declarmos el RouterLink en los imports
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
-  constructor() {
 
+export class HeaderComponent {
+  searchTerm: string = '';
+  componentesFiltrados: string[] = [];
+
+  constructor(private componentSearchService: ComponentSearchService) { }
+
+  search(): void {
+    const searchSubject = new Subject<string>();
+
+    // utilizamos la funcion searchTerm para que cuando cambie el searchTerm se actualice el observable
+    searchSubject.subscribe((searchTerm) => {
+      this.componentesFiltrados = this.componentSearchService.obtenerComponentesFiltrados(
+        searchTerm
+      );
+    });
+
+    searchSubject.next(this.searchTerm);
   }
+
 }
+
+export class AppModule { }
